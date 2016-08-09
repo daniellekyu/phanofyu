ca038d292cb6cec417f36dd0d5fe404f9aabd53c
Danielle Yu <danielle_yu@apple.com>
2016-08-09 01:54:12 -0700

    fix(timeline): initial commit for timeline
    Fixes #4

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,32 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+// var sass = require('gulp-ruby-sass') 
+var notify = require("gulp-notify") 
+var bower = require('gulp-bower');
+
+var config = {
+     sassPath: './sass',
+     bowerDir: './bower_components' 
+}
+
+gulp.task('bower', function() { 
+    return bower()
+         .pipe(gulp.dest(config.bowerDir)) 
+});
 
 gulp.task('js', function() {
 
 });
 
 gulp.task('sass', function () {
-	return gulp.src('./sass/*.scss')
-		.pipe(sass().on('error', sass.logError))
+	return gulp.src(config.sassPath + '/phanofyu.scss')
+		.pipe(sass({
+             includePaths: [
+                 './sass',
+				config.bowerDir + '/bootstrap-sass/assets/stylesheets'
+             ]
+		}).on('error', sass.logError))
 		.pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
@@ -17,4 +35,4 @@ gulp.task('sass', function () {
 });
 
 
-gulp.task('default', ['sass', 'js']);
+gulp.task('default', ['bower', 'sass', 'js']);
